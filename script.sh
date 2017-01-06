9edd2f2ed6c0c3be9d82efd827f82cfa1861dc10
sranagithub <sau_rana@hotmail.com>
2017-01-06 06:00:14 -0600

    adding script file

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$1 is image name
+#$2 is container name
+$1 $2
+echo "======================" 
+echo
+docker stop $2
+docker rm $2 -f
+docker rmi $2 
+docker build -t $1 .
+echo "Container " $2 " is running at IP :172.17.0.2"
+docker run --name $2 -v /usr/src/app/source:/usr/src/app/source $1 
+
+
